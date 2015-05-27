9559b7e13a83f6e380b8e1b0ff32208838ff0b58
Jehan <jehan.tremback@gmail.com>
2015-05-27 15:25:39 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+var ip = require('ip');
+
+var myIp = ip.address() // my ip address
+
+function replace (file, replaceWith) {
+  var newFile = ''
+  var replaceCounter = 0
+  fs.readFileSync(file).toString().split('\n').forEach(function (line) {
+    line = line.toString()
+    var toReplace = line.match(/\/\* ip-replace (.*) \*\//) && line.match(/\/\* ip-replace (.*) \*\//)[1]
+    if (toReplace) {
+      line = line.replace(new RegExp(toReplace, 'g'), replaceWith)
+      replaceCounter++
+    }
+    newFile += (line + '\n')
+  })
+  console.log(`Replaced ${replaceCounter} in ${file}`)
+  fs.writeFileSync(file, newFile)
+}
+
+var config = JSON.parse(fs.readFileSync('./.ipreplaceconfig').toString())
+config.files.forEach(function (file) {
+  replace(file, myIp)
+})
